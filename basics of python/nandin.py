4c028bf2cda79168e3b258a794e7e06f2519623a
TOPGUN212116 <simnilpk77@gmail.com>
2026-05-14 09:43:35 +0530

    Add files via upload

---
+++ basics of python/nandin.py
@@ -0,0 +1,6 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python312/nandy.py3.py
+enter the sentence
+=================== RESTART: C:/Users/hp/Documents/nandini.py ==================
