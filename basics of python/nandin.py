9a78aeac6653002461a71d2e0e28d43cef30f767
TOPGUN212116 <simnilpk77@gmail.com>
2026-05-14 09:44:44 +0530

    Update nandin.py

--- basics of python/nandin.py
+++ basics of python/nandin.py
@@ -1,6 +1,36 @@
-Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python312/nandy.py3.py
-enter the sentence
-=================== RESTART: C:/Users/hp/Documents/nandini.py ==================
+print("-----------------PROGRAM TO KNOW ATOMIC MASS OF FIRST 30 ELEMENTS ---------------")
+a=1
+while (a>0):
+    ele_nme=input("Enter the Name of the element: ")
+    atm_no=int(input("Enter the 'ATOMIC NUMBER': "))
+    L3=["Argon","Titanium","Chromium","Iron"] 
+    L4=["Scandium","Nickel"] 
+    L5=["Vanadium","Manganese","Cobalt","Copper","Zinc"] 
+    evenorodd=atm_no%2
+    if ele_nme=="hydrogen":
+        print("The",ele_nme,"'s","ATOMIC MASS IS :","1")
+        print("----------------------------------------------------------------------------------------------------------------------------------")
+    elif ele_nme=="Beryllium":
+        print("The",ele_nme,"'s","ATOMIC MASS IS :", atm_no*2+1)
+        print("----------------------------------------------------------------------------------------------------------------------------------")
+    elif ele_nme=="Nitrogen":
+        print("The",ele_nme,"'s","ATOMIC MASS IS :", atm_no*2)
+        print("----------------------------------------------------------------------------------------------------------------------------------")
+    elif ele_nme in L3:
+        print("The",ele_nme,"'s","ATOMIC MASS IS :", atm_no*2+4)
+        print("----------------------------------------------------------------------------------------------------------------------------------")
+    elif ele_nme in L4:
+        print("The",ele_nme,"'s","ATOMIC MASS IS :", atm_no*2+3)
+        print("----------------------------------------------------------------------------------------------------------------------------------")
+    elif ele_nme in L5:
+        print("The",ele_nme,"'s","ATOMIC MASS IS :", atm_no*2+5)
+        print("----------------------------------------------------------------------------------------------------------------------------------")
+    elif evenorodd==0:
+        print("The",ele_nme,"'s","ATOMIC MASS IS :", atm_no*2)
+        print("----------------------------------------------------------------------------------------------------------------------------------")
+    elif evenorodd==1:
+        print("The",ele_nme,"'s","ATOMIC MASS IS :", atm_no*2+1)
+        print("----------------------------------------------------------------------------------------------------------------------------------")
+    else:
+        print("Enter the value of ATOMIC NUMBER only in 'integer")
+
